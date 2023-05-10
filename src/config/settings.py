13dca3ad65959470d8f96efa9c56f2a937b29aab
0xshodan <hngdean@gmail.com>
2023-05-10 05:35:58 +0300

    fix catalog

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,12 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-
+SECRET_KEY = "adasdasdasdsa"
+# os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS: list[str] = ["rippo.ru", "www.rippo.ru"]
+# "rippo.ru", "www.rippo.ru"
+ALLOWED_HOSTS: list[str] = []
 AVITO_CLIENT_ID = os.getenv("CLIENT_ID")
 AVITO_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
